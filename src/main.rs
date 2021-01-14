6a3190c755ca9a4ab1e2323b251a6c5e042148f2
Jacob Lundgren <jacob.a.lundgren@gmail.com>
2021-01-14 02:15:31 +0000

    4: Pause or quit program from keyboard
    **  Behavioral change: Main event loop listens for the 'q' and 'p' keys
    which quit and toggle a pause in stepping respectively.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,9 @@ mod game_state;
 mod player;
 mod resource;
 
-use std::{error::Error, io, thread, time};
-use termion::{input::MouseTerminal, raw::IntoRawMode, screen::AlternateScreen};
-use tui::{
-    backend::TermionBackend,
-    layout::{Constraint, Layout},
-    Terminal,
-};
+use std::{error::Error, io, io::Read, thread, time::Duration, };
+use termion::{async_stdin, input::MouseTerminal, raw::IntoRawMode, screen::AlternateScreen, };
+use tui::{backend::TermionBackend, layout::{Constraint, Layout}, Terminal, };
 
 use game_state::GameState;
 
@@ -18,10 +14,26 @@ fn main() -> Result<(), Box<dyn Error>> {
     let stdout = AlternateScreen::from(stdout);
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
+
+    let mut stdin = async_stdin().bytes();
+
     let mut state = GameState::new(2);
-    for _ in 0..10 {
-        state.step();
-        thread::sleep(time::Duration::from_millis(200));
+    let mut paused = false;
+    let mut counter = 0;
+    'outer: loop {
+        for b in &mut stdin {
+            if let Ok(b'q') = b {
+                break 'outer;
+            } else if let Ok(b'p') = b {
+                paused = !paused;
+            }
+        }
+        if !paused {
+            counter = (counter + 1) % 10;
+            if counter == 0 {
+                state.step();
+            }
+        }
         terminal.draw(|f| {
             let rects = Layout::default()
                 .constraints([Constraint::Percentage(100)].as_ref())
@@ -30,6 +42,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             let t = state.as_table();
             f.render_widget(t, rects[0]);
         })?;
+        thread::sleep(Duration::from_millis(20));
     }
     Ok(())
 }
