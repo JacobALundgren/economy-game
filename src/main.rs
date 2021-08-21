0ef094cac59b90f73dd8e026e3c1b80eadde2c17
Leo K <queleok@gmail.com>
2021-08-21 14:52:50 +0300

    modified initial setup: 0 player starts with all workers idle, 1 player starts with all workers distributed between the resources

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,18 @@ mod visualization;
 
 use client::run_client;
 use game_state::GameState;
+use game_state::GameAction;
+use resource::Resource;
+use enum_iterator::IntoEnumIterator;
 
 fn main() {
     let mut state = GameState::new();
     state.register_player();
     state.register_player();
+
+    for res in Resource::into_enum_iter() {
+        state.handle_action(GameAction::AllocateWorker(1, res));
+    };
 
     run_client(&mut state, 0);
 }
