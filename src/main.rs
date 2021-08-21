0f2570a1b851f235733e7d5e6572793b9c345eb6
Leo K <queleok@gmail.com>
2021-08-21 17:47:33 +0300

    ran the autoformatter

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@ mod sell;
 mod visualization;
 
 use client::run_client;
-use game_state::GameState;
-use game_state::GameAction;
-use resource::Resource;
 use enum_iterator::IntoEnumIterator;
+use game_state::GameAction;
+use game_state::GameState;
+use resource::Resource;
 
 fn main() {
     let mut state = GameState::new();
@@ -20,7 +20,7 @@ fn main() {
 
     for res in Resource::into_enum_iter() {
         state.handle_action(GameAction::AllocateWorker(1, res));
-    };
+    }
 
     run_client(&mut state, 0);
 }
